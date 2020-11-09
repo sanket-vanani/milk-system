5d23addd3a7552ba74789db9f29fb9c7742fe7b3
sanket-vanani <sankethv8509@gmail.com>
2020-11-09 10:07:06 +0530

    db change

--- db/milksystem.sql
+++ db/milksystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 12, 2020 at 12:45 PM
+-- Generation Time: Nov 09, 2020 at 05:36 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -36,13 +36,6 @@ CREATE TABLE `Admin` (
   `updatedAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `Admin`
---
-
-INSERT INTO `Admin` (`id`, `userName`, `password`, `createdAt`, `updatedAt`) VALUES
-(1, 'admin', 'admin@123', NULL, NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -52,17 +45,25 @@ INSERT INTO `Admin` (`id`, `userName`, `password`, `createdAt`, `updatedAt`) VAL
 CREATE TABLE `Customer` (
   `id` int(11) NOT NULL,
   `customerName` varchar(255) NOT NULL,
-  `phoneNumber` int(11) NOT NULL,
+  `phoneNumber` varchar(255) NOT NULL,
   `fatherName` varchar(255) DEFAULT NULL,
   `accountNumber` varchar(255) DEFAULT NULL,
   `IFSCCode` varchar(255) DEFAULT NULL,
   `bankName` varchar(255) DEFAULT NULL,
   `villageName` varchar(255) DEFAULT NULL,
   `address` text,
+  `memberType` varchar(255) NOT NULL,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
   `DairyId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Customer`
+--
+
+INSERT INTO `Customer` (`id`, `customerName`, `phoneNumber`, `fatherName`, `accountNumber`, `IFSCCode`, `bankName`, `villageName`, `address`, `memberType`, `createdAt`, `updatedAt`, `DairyId`) VALUES
+(2, 'Customer Test', '784664125', 'Customer father name', '54567757564', 'dfgdgsr', 'Bank Name', 'Village Name', 'Address line', 'buyer', '2020-11-07 05:36:29', '2020-11-07 05:36:29', 1);
 
 -- --------------------------------------------------------
 
@@ -82,6 +83,13 @@ CREATE TABLE `Dairy` (
   `updatedAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `Dairy`
+--
+
+INSERT INTO `Dairy` (`id`, `userName`, `password`, `centerName`, `dairyName`, `dairyAddress`, `phoneNumber`, `createdAt`, `updatedAt`) VALUES
+(1, 'sanket', '81c6fa745434534c4a9633ad8c08506a', 'surat', 'sumul', 'Sumul road', '8347583112', '2020-11-07 05:35:07', '2020-11-07 05:35:07');
+
 -- --------------------------------------------------------
 
 --
@@ -91,6 +99,7 @@ CREATE TABLE `Dairy` (
 CREATE TABLE `LocalSale` (
   `id` int(11) NOT NULL,
   `saleDate` varchar(255) NOT NULL,
+  `timeslot` varchar(255) NOT NULL,
   `animalType` varchar(255) NOT NULL,
   `liter` varchar(255) NOT NULL,
   `rate` varchar(255) NOT NULL,
@@ -100,6 +109,13 @@ CREATE TABLE `LocalSale` (
   `DairyId` int(11) DEFAULT NULL,
   `CustomerId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `LocalSale`
+--
+
+INSERT INTO `LocalSale` (`id`, `saleDate`, `timeslot`, `animalType`, `liter`, `rate`, `totalAmount`, `createdAt`, `updatedAt`, `DairyId`, `CustomerId`) VALUES
+(2, 'current Date', 'morning', 'animal type', '23', '23', '2314', '2020-11-07 05:38:30', '2020-11-07 05:38:30', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -122,6 +138,14 @@ CREATE TABLE `MilkCollection` (
   `CustomerId` int(11) DEFAULT NULL,
   `DairyId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `MilkCollection`
+--
+
+INSERT INTO `MilkCollection` (`id`, `liter`, `fat`, `snf`, `rate`, `amount`, `addDate`, `timeslot`, `animalType`, `createdAt`, `updatedAt`, `CustomerId`, `DairyId`) VALUES
+(1, '1.2', '23', 'cd34', '65', '3252', '09-09-2020', 'morning', 'cow', '2020-11-07 06:01:48', '2020-11-07 06:01:48', 2, 1),
+(2, '1.2', '23', 'cd34', '65', '3252', '10-09-2020', 'morning', 'cow', '2020-11-07 06:12:05', '2020-11-07 06:12:05', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -197,37 +221,37 @@ ALTER TABLE `Payment`
 -- AUTO_INCREMENT for table `Admin`
 --
 ALTER TABLE `Admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `Customer`
 --
 ALTER TABLE `Customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `Dairy`
 --
 ALTER TABLE `Dairy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `LocalSale`
 --
 ALTER TABLE `LocalSale`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `MilkCollection`
 --
 ALTER TABLE `MilkCollection`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `Payment`
 --
 ALTER TABLE `Payment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
